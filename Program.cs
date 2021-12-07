157ae658cf65c5c47984b6ab78ce236522731902
Katherine Winters <goble.katherine@gmail.com>
2021-12-06 19:37:21 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your best friend's name?");
+            var friendsName = Console.ReadLine();
+
+            Console.WriteLine("How did you meet your best friend?");
+            var meetFriend = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite thing to do with your best friend?");
+            var thingToDo = Console.ReadLine();
+
+
+            Console.WriteLine($"My best friends name is {friendsName}.");
+            Console.WriteLine($"I meet my best friend {meetFriend}.");
+            Console.WriteLine($"My favorite thing to do with my best friend is {thingToDo}.");
+         
+        
+            
+
+        }
+    }
+}
